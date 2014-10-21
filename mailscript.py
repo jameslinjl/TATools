433732d4d2b460962ff35448dcd094ab82f1d11f
James Lin <jl3782@columbia.edu>
2014-10-21 04:30:50 -0400

    added unicode to string

--- mailscript.py
+++ mailscript.py
@@ -14,17 +14,17 @@ wks = gc.open(filename).sheet1
 
 #Download
 print "Downloading Data Ranges..."
-recvaddrlst = wks.range('B2:B3')
-list1 = wks.range('C2:C3')
-list2 = wks.range('D2:D3')
-list3 = wks.range('E2:E3')
-list4 = wks.range('F2:F3')
-list5 = wks.range('G2:G3')
-list6 = wks.range('H2:H3')
-list7 = wks.range('I2:I3')
-list8 = wks.range('J2:J3')
-list9 = wks.range('K2:K3')
-comment_list = wks.range('L2:L3')
+recvaddrlst = wks.range('B104:B199')
+list1 = wks.range('C104:C199')
+list2 = wks.range('D104:D199')
+list3 = wks.range('E104:E199')
+list4 = wks.range('F104:F199')
+list5 = wks.range('G104:G199')
+list6 = wks.range('H104:H199')
+list7 = wks.range('I104:I199')
+list8 = wks.range('J104:J199')
+list9 = wks.range('K104:K199')
+comment_list = wks.range('L104:L199')
 
 #Start email script
 print "Start sending emails"
@@ -41,22 +41,22 @@ fromaddr = raw_input('Sender Name: ')
 server.login(username, password)
 
 #Write actual email
-subject = "HW1 Comments"
+subject = "HW2 Comments"
 i = 0
 while i < len(recvaddrlst):
 	recvaddr = str(recvaddrlst[i].value) + "@columbia.edu"
 	msg = "From : %s\nTo: %s\nSubject: %s\n" %(fromaddr, recvaddr, subject)
-	msg = msg + "Hi!\n\nHere is your HW1 Grade Report. If you have any questions, please email cs3134staff@lists.cs.columbia.edu\n\n"
+	msg = msg + "Hi!\n\nWe are testing out this new email script, so please bear with any kinks that might still be here. Here is your HW2 Grade Report. If you have any questions, please email cs3134staff@lists.cs.columbia.edu. The grading rubric and HW3 clarifications will be out soon.\n\n"
 	msg = msg + "Written 1: " + str(list1[i].value) + "/10\n"
-	msg = msg +	"Written 2: " + str(list2[i].value) + "/10\n"
-	msg = msg + "Written 3: " + str(list3[i].value) + "/10\n"
+	msg = msg +	"Written 2: " + str(list2[i].value) + "/5\n"
+	msg = msg + "Written 3: " + str(list3[i].value) + "/5\n"
 	msg = msg + "Written 4: " + str(list4[i].value) + "/10\n"
-	msg = msg + "Written 5: " + str(list5[i].value) + "/10\n"
-	msg = msg + "Written 6: " + str(list6[i].value) + "/10\n"
-	msg = msg + "Program 1: " + str(list7[i].value) + "/10\n"
-	msg = msg + "Program 2: " + str(list8[i].value) + "/10\n"
-	msg = msg + "Program 3: " + str(list9[i].value) + "/10\n\n"
-	msg = msg + "Comments: " + comment_list[i].value + "\n\n\n"
+	msg = msg + "Written 5: " + str(list5[i].value) + "/5\n"
+	msg = msg + "Written 6: " + str(list6[i].value) + "/5\n"
+	msg = msg + "Program 1: " + str(list7[i].value) + "/20\n"
+	msg = msg + "Program 2: " + str(list8[i].value) + "/20\n"
+	msg = msg + "Program 3: " + str(list9[i].value) + "/20\n\n"
+	msg = msg + "Comments: \n" + (comment_list[i].value).encode('utf-8') + "\n\n\n"
 	msg = msg + "--James Lin, Head TA"
 	i = i + 1
 
--- sheet_to_comments.py
+++ sheet_to_comments.py
@@ -14,19 +14,19 @@ wks = gc.open(filename).sheet1
 
 #Download
 print "Downloading Data Ranges..."
-list1 = wks.range('C2:C252')
-list2 = wks.range('D2:D252')
-list3 = wks.range('E2:E252')
-list4 = wks.range('F2:F252')
-list5 = wks.range('G2:G252')
-list6 = wks.range('H2:H252')
-list7 = wks.range('I2:I252')
-list8 = wks.range('J2:J252')
-list9 = wks.range('K2:K252')
-comment_list = wks.range('L2:L252')
+list1 = wks.range('C2:C199')
+list2 = wks.range('D2:D199')
+list3 = wks.range('E2:E199')
+list4 = wks.range('F2:F199')
+list5 = wks.range('G2:G199')
+list6 = wks.range('H2:H199')
+list7 = wks.range('I2:I199')
+list8 = wks.range('J2:J199')
+list9 = wks.range('K2:K199')
+comment_list = wks.range('L2:L199')
 
 #Switch into correct directory and get list of directories
-os.chdir("./Homework 1")
+os.chdir("./Homework 2-1")
 dirList = os.listdir("./")
 
 #Write to file
@@ -36,15 +36,15 @@ while i < len(list1):
 	os.chdir("./" + str(dirList[i+1]))
 	with open("comments.txt", "w") as myfile:
 		myfile.write("Written 1: " + str(list1[i].value) + "/10\n")
-		myfile.write("Written 2: " + str(list2[i].value) + "/10\n")
-		myfile.write("Written 3: " + str(list3[i].value) + "/10\n")
+		myfile.write("Written 2: " + str(list2[i].value) + "/5\n")
+		myfile.write("Written 3: " + str(list3[i].value) + "/5\n")
 		myfile.write("Written 4: " + str(list4[i].value) + "/10\n")
-		myfile.write("Written 5: " + str(list5[i].value) + "/10\n")
-		myfile.write("Written 6: " + str(list6[i].value) + "/10\n")
-		myfile.write("Program 1: " + str(list7[i].value) + "/10\n")
-		myfile.write("Program 2: " + str(list8[i].value) + "/10\n")
-		myfile.write("Program 3: " + str(list9[i].value) + "/10\n\n")
+		myfile.write("Written 5: " + str(list5[i].value) + "/5\n")
+		myfile.write("Written 6: " + str(list6[i].value) + "/5\n")
+		myfile.write("Program 1: " + str(list7[i].value) + "/20\n")
+		myfile.write("Program 2: " + str(list8[i].value) + "/20\n")
+		myfile.write("Program 3: " + str(list9[i].value) + "/20\n\n")
 
-		myfile.write("Comments: " + comment_list[i].value)
+		myfile.write("Comments: " + (comment_list[i].value).encode('utf-8'))
 	os.chdir("..")
 	i = i + 1
