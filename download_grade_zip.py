d2c7bdfdd7a8a7e94fd8b1ba6409e6a825d6293c
James Lin <jl3782@columbia.edu>
2015-01-14 20:27:28 -0500

    selenium script working hooray

--- download_grade_zip.py
+++ download_grade_zip.py
@@ -1,64 +1,74 @@
 #!/usr/bin/env python
 
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-username = raw_input('UNI: ')
-password = raw_input('Password: ')
+#create firefox profile for auto download and save
+def get_firefox_profile():
 
-#get to the initial page
-driver = webdriver.Chrome('/Users/jameslin/chromedriver')
-driver.get("https://courseworks.columbia.edu/portal/login")
-assert "Columbia University Authentication" in driver.title
+    fp = webdriver.FirefoxProfile()
 
-#type username into correct field
-usern = driver.find_element_by_name("username")
-usern.send_keys(username)
+    fp.set_preference("browser.download.folderList",2)
+    fp.set_preference("browser.download.manager.showWhenStarting",False)
+    fp.set_preference("browser.download.dir", os.getcwd())
+    fp.set_preference("browser.helperApps.neverAsk.saveToDisk", 
+        "application/zip")
+    return fp
 
-#type password into correct field
-passw = driver.find_element_by_name("password")
-passw.send_keys(password)
+def run_driver(prof, uname, pword):
+    #get to the initial page
+    #chrome was causing issues for iFrames
+    driver = webdriver.Firefox(firefox_profile=prof)
+    driver.get("https://courseworks.columbia.edu/portal/login")
 
-#click submit
-submit = driver.find_element_by_name("submit")
-submit.click()
+    #type username into correct field
+    usern = driver.find_element_by_name("username")
+    usern.send_keys(uname)
 
-#open to assignments page
-ds_cw_path = "https://courseworks.columbia.edu/portal/site/COMSW3134_001_2014_3/"
-assignments_path = "page/af109703-a9a8-4808-a580-277555af3252"
-driver.get(ds_cw_path)
-driver.get(ds_cw_path + assignments_path)
+    #type password into correct field
+    passw = driver.find_element_by_name("password")
+    passw.send_keys(pword)
 
-#switch into iframe
-#open to homework 4 grades
-driver.switch_to.frame("Mainbf3d7569xad7fx43efx858bxc3fbf2fae0a3")
-grade_button = driver.find_element_by_xpath("//form[@name='listAssignmentsForm']/table[1]/tbody[1]/tr[4]/td[2]/div[1]/a[3]")
-grade_button.click()
-self.selenium.wait_for_page_to_load("30000")
+    #click submit
+    submit = driver.find_element_by_name("submit")
+    submit.click()
 
-#download all link
-download_all = driver.find_element_by_xpath("//form[@name='viewForm']/div[@class='listNav']/a[1]")
-download_all.click()
+    #open to assignments page
+    ds_cw_path = "https://courseworks.columbia.edu/portal/site/COMSW3134_001_2014_3/"
+    assignments_path = "page/af109703-a9a8-4808-a580-277555af3252"
+    driver.get(ds_cw_path)
+    driver.get(ds_cw_path + assignments_path)
 
-#download grade file (maybe more later on?)
-grade_cbox = driver.find_element_by_xpath("//input[@id='gradeFile']")
-grade_cbox.click()
-download_button = driver.find_element_by_xpath("//input[@name='downloadButton']")
-download_button.click()
-real_download_button = driver.find_element_by_xpath("//div[@class='messageInformation']/p[1]/a[1]")
-real_download_button.click()
+    #switch into iframe
+    #open to homework 4 grades
+    driver.switch_to.frame("Mainbf3d7569xad7fx43efx858bxc3fbf2fae0a3")
+    grade_button = driver.find_element_by_xpath("//form[@name='listAssignmentsForm']/table/tbody/tr[2]/td[2]/div/a[3]")
+    grade_button.click()
 
-#there might be a confirmation for some browsers
+    #download all link
+    download_all = driver.find_element_by_xpath("//form[@name='viewForm']/div[@class='listNav']/a[1]")
+    download_all.click()
 
-sleep(5)
-driver.close()
+    #download grade file (maybe more later on?)
+    grade_cbox = driver.find_element_by_xpath("//input[@id='gradeFile']")
+    grade_cbox.click()
+    download_button = driver.find_element_by_xpath("//input[@name='downloadButton']")
+    download_button.click()
+    real_download_button = driver.find_element_by_xpath("//div[@class='messageInformation']/p[1]/a[1]")
+    real_download_button.click()
 
+    driver.close()
 
+def main():
+    
+    username = raw_input('UNI: ')
+    password = raw_input('Password: ')
 
+    profile = get_firefox_profile()
 
+    run_driver(profile, username, password)
 
-
-
-
-
+if __name__ == '__main__':
+    main()
